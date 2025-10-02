ff136c8311fd67118b0fb962bbb4ab60e92b7dc1
sharifulislamudoy <surifroton301@gmail.com>
2025-10-02 14:37:53 +0600

    implement simple bug fix

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Hero from './Hero/Hero';
+import { ReTitle } from 're-title';
 
 const Home = () => {
     return (
         <div>
+            <ReTitle title='Career Crafter'/>
             <Hero></Hero>
         </div>
     );
